16fffb0a8834bc0ae3578a9c0079dd66848ef213
mMormin <mmboudot@gmail.com>
2023-08-25 15:11:28 +0200

    db export

--- app/db.js
+++ app/db.js
@@ -1,3 +1,5 @@
 const { Client } = require("pg");
 const db = new Client("postgresql://etudiant:js4life@pg.oclock.lan/trombi");
 db.connect().then(console.log("Connecté à la DB !"));
+
+module.exports = db;
