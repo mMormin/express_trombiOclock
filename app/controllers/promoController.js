413070515a077ae4fedd3adb08242701f2c1ba14
mMormin <mmboudot@gmail.com>
2023-08-27 19:00:39 +0200

    :hundred_points: ALL Promises then/catch into async/await

--- app/controllers/promoController.js
+++ app/controllers/promoController.js
@@ -1,24 +1,23 @@
-//const promos = require("../docs/data/promos.json");
-
 const db = require("../db.js");
 
 const promoController = {
-  homePage: (req, res) => {
+  homePage: (res) => {
     res.render("index");
   },
-  promosListPage: (req, res, next) => {
-    const query = "SELECT * FROM promo ORDER BY name ASC;"
-    db.query(query)
-      .then((result) => {
-        const promos = result.rows;
-        res.render("promosList", { promos });
-      })
-      .catch((error) => {
-        res.locals.error = { code: 404, message: error };
-        next();
-      });
+  promosListPage: async (res, next) => {
+    const query = "SELECT * FROM promo ORDER BY name ASC;";
+
+    try {
+      const promos = await db.query(query).rows;
+
+      res.render("promosList", { promos });
+    } catch (error) {
+      res.locals.error = { code: 404, message: error };
+
+      return next();
+    }
   },
-  promoPage: (req, res, next) => {
+  promoPage: async (req, res, next) => {
     const { id } = req.params;
 
     const query = {
@@ -26,15 +25,15 @@ const promoController = {
       values: [id],
     };
 
-    db.query(query)
-      .then((result) => {
-        const promo = result.rows[0];
-        res.render("promo", { promo });
-      })
-      .catch((error) => {
-        res.locals.error = { code: 404, message: error };
-        next();
-      });
+    try {
+      const promo = await db.query(query).rows[0];
+
+      res.render("promo", { promo });
+    } catch (error) {
+      res.locals.error = { code: 404, message: error };
+
+      return next();
+    }
   },
 };
 
--- app/controllers/studentController.js
+++ app/controllers/studentController.js
@@ -1,71 +1,60 @@
-//const promos = require("../../docs/data/promos.json");
-//const students = require("../../docs/data/students.json");
-
 const db = require("../db.js");
 
 const studentController = {
-  studentsByPromoPage: (req, res, next) => {
+  studentsByPromoPage: async (req, res, next) => {
     const { id } = req.params;
     const promoId = Number(id);
-    studentController.getPromoById(promoId).then((promo) => {
-      studentController.getStudentsByPromo(promoId).then((students) => {
-        res.render("students", { students, promo });
-      })
 
-      
-    });
+    const promoIdPromise = studentController.getPromoById(promoId);
+    const studentsByPromoPromise = studentController.getPromoById(promoId);
+    const promises = [promoIdPromise, studentsByPromoPromise];
 
-    // if (!promo) {
-    //   res.locals.error = {
-    //     code: 404,
-    //     message: "Aucune promo correspondante !",
-    //   };
-    //   return next();
-    // }
+    try {
+      const results = await Promise.allSettled(promises);
 
+      // Destructuring
+      const [{value:promo}, {value: students}] = results;
 
-    // if (!studentList.length) {
-    //   res.locals.error = {
-    //     code: 404,
-    //     message: "Aucune éléve trouvé !",
-    //   };
-    //   return next();
-    // }
+      // À l'ancienne le couz
+      // const promo = results[0].value;
+      // const students = results[1].value;
+
+      res.render("students", { promo, students });
+    } catch (error) {
+      res.locals.error = { code: 404, message: error };
+
+      return next();
+    }
   },
 
-  getPromoById: (promoId) => {
-    let promo;
-
+  getPromoById: async (promoId) => {
     const query = {
       text: "SELECT * FROM promo WHERE id = $1",
       values: [promoId],
     };
 
-    return db
-      .query(query)
-      .then((result) => {
-        promo = result.rows[0];
-        return promo;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    try {
+      const result = await db.query(query);
+
+      return result.rows[0];
+    } catch (error) {
+      console.log(error);
+    }
   },
 
-  getStudentsByPromo: (promoId) => {
+  getStudentsByPromo: async (promoId) => {
     const query = {
       text: "SELECT * FROM student WHERE promo_id = $1 ORDER BY first_name ASC",
       values: [promoId],
     };
 
-    return db.query(query)
-      .then((result) => {
-        const students = result.rows;
-        return students;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    try {
+      const result = await db.query(query);
+
+      return result.rows;
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
 
